9aed1a3d01ecdb83910a12396a0321421dc0bd8b
cameron-keene <74378283+cameron-keene@users.noreply.github.com>
2022-03-27 18:14:45 -0400

    testing

--- BEAT_PROJECT/mysite/b_project/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BProjectConfig(AppConfig):
-    name = 'b_project'
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,7 @@
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path('app/', include('b_project.urls')),
     path('admin/', admin.site.urls),
 ]
