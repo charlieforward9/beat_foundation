3269d8469e8178322e8ee83861e4822f6996a292
Thomas Pena <60591404+thomaspr1205@users.noreply.github.com>
2022-04-06 17:36:00 -0400

    Add files via upload

--- BEAT_PROJECT/myapp/forms.py
+++ BEAT_PROJECT/myapp/forms.py
@@ -13,33 +13,6 @@ class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-# class User(models.Model):
-#     username = models.CharField(max_length=100)
-#     def _str__(self):
-#         return self.username
-
-
-# class RegistrationForn(ModelForm):
-#     username = forms.CharField(max_length=100)
-#     password = forms.CharField(widget=PasswordInput)
-#     email = forms.CharField(widget=EmailInput)
-#     HRdata = forms.FileField()
-#     CalendarData = forms.FileField()
-#     class Meta:
-#         model = User
-#         fields = ["username", "password", "email", "HRdata", "CalendarData"]
-
-
-
-
-# class RegisterForm(UserCreationForm):
-#     # HRdata = forms.FileField()
-#     # CalendarData = forms.FileField()
-
-#     class Meta:
-#         model = User
-#         fields = ["username", "email", "password", "fisrt_name", "last_name"]
-
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
@@ -54,63 +27,88 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
 
-# class SignUpForm(UserCreationForm):
-#     email = forms.EmailField(required=True)
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email', 'password1', 'password2']
-#         def clean_password2(self):
-#             cd = self.cleaned_data
-#             if cd['password'] != cd['password2']:
-#                 raise forms.ValidationError('Passwords don\'t match.')
-#             return cd['password2']
-
-class SignUpForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({
-            'class': 'form-input',
-            'required':'',
-            'name':'username',
-            'id':'username',
-            'type':'text',
-            'placeholder':'John Doe',
-            'maxlength': '16',
-            'minlength': '6',
-            })
-        self.fields['email'].widget.attrs.update({
-            'class': 'form-input',
-            'required':'',
-            'name':'email',
-            'id':'email',
-            'type':'email',
-            'placeholder':'JohnDoe@mail.com',
-            })
-        self.fields['password1'].widget.attrs.update({
-            'class': 'form-input',
-            'required':'',
-            'name':'password1',
-            'id':'password1',
-            'type':'password',
-            'placeholder':'password',
-            'maxlength':'22',
-            'minlength':'8'
-            })
-        self.fields['password2'].widget.attrs.update({
-            'class': 'form-input',
-            'required':'',
-            'name':'password2',
-            'id':'password2',
-            'type':'password',
-            'placeholder':'password',
-            'maxlength':'22',
-            'minlength':'8'
-            })
-
-
-    username = forms.CharField(max_length=20, label=False)
-    email = forms.EmailField(max_length=100)
-
-    class Meta:
-        model = User
+class SignUpForm(UserCreationForm): 
+    def __init__(self, *args, **kwargs): 
+        super().__init__(*args, **kwargs) 
+        self.fields['username'].widget.attrs.update({ 
+            'class': 'form-input', 
+            'required':'', 
+            'name':'username', 
+            'id':'username', 
+            'type':'text', 
+            'placeholder':'John Doe', 
+            'maxlength': '16', 
+            'minlength': '6', 
+            }) 
+        self.fields['email'].widget.attrs.update({ 
+            'class': 'form-input', 
+            'required':'', 
+            'name':'email', 
+            'id':'email', 
+            'type':'email', 
+            'placeholder':'JohnDoe@mail.com', 
+            }) 
+        self.fields['password1'].widget.attrs.update({ 
+            'class': 'form-input', 
+            'required':'', 
+            'name':'password1', 
+            'id':'password1', 
+            'type':'password', 
+            'placeholder':'password', 
+            'maxlength':'22',  
+            'minlength':'8' 
+            }) 
+        self.fields['password2'].widget.attrs.update({ 
+            'class': 'form-input', 
+            'required':'', 
+            'name':'password2', 
+            'id':'password2', 
+            'type':'password', 
+            'placeholder':'password', 
+            'maxlength':'22',  
+            'minlength':'8' 
+            }) 
+ 
+ 
+    username = forms.CharField(max_length=20, label=False) 
+    email = forms.EmailField(max_length=100) 
+ 
+    class Meta: 
+        model = User 
         fields = ('username', 'email', 'password1', 'password2', )
+
+class form1(forms.Form):
+    activity = forms.CharField()
+    start = forms.DateTimeField(widget=DatePickerInput)
+    end = forms.DateTimeField(widget=DatePickerInput)
+    avg = forms.BooleanField(required=False)
+    high = forms.BooleanField(required=False)
+    low = forms.BooleanField(required=False)
+
+class form2(forms.Form):
+    activity1 = forms.CharField()
+    activity2 = forms.CharField()
+    start = forms.DateTimeField(widget=DatePickerInput)
+    end = forms.DateTimeField(widget=DatePickerInput)
+    avg = forms.BooleanField(required=False)
+    high = forms.BooleanField(required=False)
+    low = forms.BooleanField(required=False)
+
+class form3(forms.Form):
+    activity = forms.CharField()
+    start = forms.DateTimeField(widget=DatePickerInput)
+    end = forms.DateTimeField(widget=DatePickerInput)
+    avg = forms.BooleanField(required=False)
+    high = forms.BooleanField(required=False)
+    low = forms.BooleanField(required=False)
+
+class form4(forms.Form):
+    start = forms.DateTimeField(widget=DatePickerInput)
+    end = forms.DateTimeField(widget=DatePickerInput)
+    avg = forms.BooleanField(required=False)
+    high = forms.BooleanField(required=False)
+    low = forms.BooleanField(required=False)
+
+class form5(forms.Form):
+    start = forms.DateTimeField(widget=DatePickerInput)
+    end = forms.DateTimeField(widget=DatePickerInput)
