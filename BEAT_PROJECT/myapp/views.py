0f44c6c1b077aafc27a5b09c2816ec8ecdcc6789
cameron-keene <74378283+cameron-keene@users.noreply.github.com>
2022-04-13 19:05:07 -0400

    Trend One
    Trend One fully functional with correct query

--- BEAT_PROJECT/myapp/views.py
+++ BEAT_PROJECT/myapp/views.py
@@ -10,6 +10,7 @@ import urllib, base64
 from plotly.offline import plot
 import plotly.graph_objects as go
 from .forms import form1, form2, form3, form4, form5
+from datetime import datetime, timedelta
 
 # Login imports
 from .forms import LoginForm, SignUpForm
@@ -287,10 +288,112 @@ def trend1(request):
             print("High:", high) 
             print("Low:", low) 
             print("========================================")
+            # set the username
+            if request.user.get_username() == 'Charbo':
+                userid = '0.8302870117189518'
+            else: # Cam
+                userid = '0.26777655249889387'
+
+            # print("userid: ", userid)
 
             # Query
-            
+            activity = activity.lower()
+            # print("Activity:", activity)
             # Graph 
+            date_time = start.strftime("%Y-%m-%d %H:%M:%S")
+            date_time = date_time[0:10]
+            print(date_time)
+            all_days = []
+            avg_days = []
+            min_days = []
+            max_days = []
+            day_start_test = date_time + ' 00:00:00'
+            day_end_test = date_time + ' 23:59:59'
+            # print(day_start_test)
+            # print(day_end_test)
+
+
+
+            real_query = """SELECT crichardson5.beat_event.cat, ROUND(AVG(CRICHARDSON5.beat_heartrate.HRVALUE)) AS AVG_HR, MIN(CRICHARDSON5.beat_heartrate.HRVALUE) as MIN_HR, MAX(CRICHARDSON5.beat_heartrate.HRVALUE) AS MAX_HR
+                            FROM CRICHARDSON5.beat_event JOIN CRICHARDSON5.beat_heartrate
+                            ON (
+                                crichardson5.beat_event.USERID = crichardson5.beat_heartrate.USERID AND
+                                crichardson5.beat_heartrate.TIME_STAMP BETWEEN crichardson5.beat_event.TSTART AND crichardson5.beat_event.TEND
+                                )
+                            WHERE crichardson5.beat_event.USERID = %s AND
+                                crichardson5.beat_event.TSTART BETWEEN %s AND %s
+                            GROUP BY crichardson5.beat_event.cat"""
+
+
+            cursor.execute(real_query, (userid, day_start_test, day_end_test,))
+            # cursor.execute(test_query_1,(test_userid, test_day_start, test_day_end,))
+            day_df = pd.DataFrame(cursor, columns=['CAT','AVG_HR','MIN_HR', 'MAX_HR'])
+            if not day_df.empty:
+                # print(day_df)
+                all_days.append(date_time)
+                # test = day_df['AVG_HR']
+                # print('testing: ', day_df['AVG_HR'][0])
+                avg_days.append(day_df['AVG_HR'][0])
+                min_days.append(day_df['MIN_HR'][0])
+                max_days.append(day_df['MAX_HR'][0])
+
+
+            while start != end:
+                if start > end:
+                    break
+                start = start + timedelta(days=1)
+                date_time = start.strftime("%Y-%m-%d %H:%M:%S")
+                date_time = date_time[0:10]
+                day_start = date_time + ' 00:00:00'
+                day_end = date_time + ' 23:59:59'
+                # print(day_start)
+                # print(day_end)
+                cursor.execute(real_query, (userid, day_start, day_end,))
+                day_df = pd.DataFrame(cursor, columns=['CAT','AVG_HR','MIN_HR', 'MAX_HR'])
+                if not day_df.empty:
+                    all_days.append(date_time)
+                    avg_days.append(day_df['AVG_HR'][0])
+                    min_days.append(day_df['MIN_HR'][0])
+                    max_days.append(day_df['MAX_HR'][0])
+
+            # print(all_days)
+            # print(avg_days)
+            # print(min_days)
+            # print(max_days)
+
+            
+            graphs = []
+            groups = ['mean','min','max']
+            # add the bar graph
+            graphs.append(
+                go.Scatter(
+                    x=all_days,
+                    y=avg_days,
+                    name='Mean Heart Rate',
+                )
+            )
+            graphs.append(
+                go.Scatter(
+                    x=all_days,
+                    y=min_days,
+                    name='Min Heart Rate',
+                )
+            )
+            graphs.append(
+                go.Scatter(
+                    x=all_days,
+                    y=max_days,
+                    name='Max Heart Rate',
+                )
+            )
+            layout = {
+                'title': 'Aggregate Trends',
+                'xaxis_title': 'Max, Min, Avg',
+                'yaxis_title': 'Heart Rate',
+                'height': 600,
+                'width': 1000,
+            }
+
             # getting the low, high, and avg for three days
             # need to be format YEAR-MM-DD
             day1 = '2021-02-11'
@@ -305,11 +408,14 @@ def trend1(request):
             day3_end = day3 + ' 23:59:59'
 
             query = """SELECT * FROM CRICHARDSON5.BEAT_HEARTRATE WHERE time_stamp > %s AND time_stamp < %s"""
+
+  
+
             cursor.execute(query, (day1_start, day1_end, ))
             day1_df = pd.DataFrame(cursor, columns=['USERID','time_stamp','HRvalue'])
             day1_df = day1_df.describe()
             day1_df.drop(['count', 'std', '25%', '50%', '75%'], inplace=True)
-            print(day1_df)
+            # print(day1_df)
 
             cursor.execute(query, (day2_start, day2_end, ))
             day2_df = pd.DataFrame(cursor, columns=['USERID','time_stamp','HRvalue'])
@@ -327,37 +433,37 @@ def trend1(request):
             # print(final_df)
             
             # container for all the graphs
-            graphs = []
-            groups = ['mean','min','max']
+            # graphs = []
+            # groups = ['mean','min','max']
             # add the bar graph
-            graphs.append(
-                go.Bar(
-                    x=groups,
-                    y=day1_df['HRvalue'],
-                    name=day1,
-                )
-            )
-            graphs.append(
-                go.Bar(
-                    x=groups,
-                    y=day2_df['HRvalue'],
-                    name=day2,
-                )
-            )
-            graphs.append(
-                go.Bar(
-                    x=groups,
-                    y=day3_df['HRvalue'],
-                    name=day3,
-                )
-            )
-            layout = {
-                'title': 'Aggregate Trends',
-                'xaxis_title': 'Max, Min, Avg',
-                'yaxis_title': 'Heart Rate',
-                'height': 600,
-                'width': 1000,
-            }
+            # graphs.append(
+            #     go.Bar(
+            #         x=groups,
+            #         y=day1_df['HRvalue'],
+            #         name=day1,
+            #     )
+            # )
+            # graphs.append(
+            #     go.Bar(
+            #         x=groups,
+            #         y=day2_df['HRvalue'],
+            #         name=day2,
+            #     )
+            # )
+            # graphs.append(
+            #     go.Bar(
+            #         x=groups,
+            #         y=day3_df['HRvalue'],
+            #         name=day3,
+            #     )
+            # )
+            # layout = {
+            #     'title': 'Aggregate Trends',
+            #     'xaxis_title': 'Max, Min, Avg',
+            #     'yaxis_title': 'Heart Rate',
+            #     'height': 600,
+            #     'width': 1000,
+            # }
             # Getting HTML needed to render the plot.
             plot_div = plot({'data': graphs, 'layout': layout}, 
                             output_type='div')
